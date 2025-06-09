5e086b0bccfae70f04c668aa10d2f1841c6270a1
OuidadM <mochariqouidad@gmail.com>
2025-06-09 12:37:10 +0100

    Résolution du problème de l'affichage de Create Client

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()), // Required for HTTP requests
+    provideHttpClient(withFetch()),
+    importProvidersFrom(ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })),
   ]
-}).catch(err => console.error(err));
+});
+
